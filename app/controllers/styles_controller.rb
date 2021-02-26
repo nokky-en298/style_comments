804000788df31d45502711e8e5694b701c1d3954
nokky-en298 <enapple.tkmkt7@gmail.com>
2021-02-26 20:57:12 +0900

    Active Storageの導入

--- app/controllers/styles_controller.rb
+++ app/controllers/styles_controller.rb
@@ -1,4 +1,10 @@
 class StylesController < ApplicationController
   def index
   end
+
+
+  private
+  def style_params
+    params.require(:style).permit(:image, :title, :quality, :item, :remark).merge(user_id: current_user.id)
+  end
 end
--- app/models/style.rb
+++ app/models/style.rb
@@ -1,3 +1,4 @@
 class Style < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
 end
